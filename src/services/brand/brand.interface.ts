a2ebbc9ae1d15c115f1a5b3d19b8750c3cffa6e5
suanhsawmtung <suanhsawmtung@gmail.com>
2026-09-17 13:52:35 +0630

    fix(brand): correct type for select option

--- src/services/brand/brand.interface.ts
+++ src/services/brand/brand.interface.ts
@@ -1,19 +1,36 @@
 import { Brand } from "@prisma/client";
-import { CreateBrandParams, ListBrandResultT, ListBrandsParams, ListBrandT, ListSelectOptionBrandT, UpdateBrandParams } from "../../types/brand";
-import { ServiceResponseT } from "../../types/common";
+import {
+  CreateBrandParams,
+  ListBrandResultT,
+  ListBrandsParams,
+  ListBrandT,
+  UpdateBrandParams,
+} from "../../types/brand";
+import { SelectOptionT, ServiceResponseT } from "../../types/common";
 
 export interface IBrandService {
-  listPublicBrands(): Promise<ServiceResponseT<ListSelectOptionBrandT[]>>;
-  selectOptionListBrands(query: { limit?: number; cursor?: number | null; search?: string | undefined }): Promise<ServiceResponseT<{ 
-    items: ListSelectOptionBrandT[], 
-    nextCursor: number | null
-  }>>;
+  listPublicBrands(): Promise<ServiceResponseT<SelectOptionT[]>>;
+  selectOptionListBrands(query: {
+    limit?: number;
+    cursor?: number | null;
+    search?: string | undefined;
+  }): Promise<
+    ServiceResponseT<{
+      items: SelectOptionT[];
+      nextCursor: number | null;
+    }>
+  >;
 }
 
 export interface IAdminBrandService {
-  listBrands(params: ListBrandsParams): Promise<ServiceResponseT<ListBrandResultT>>;
+  listBrands(
+    params: ListBrandsParams,
+  ): Promise<ServiceResponseT<ListBrandResultT>>;
   getBrandDetail(slug: string): Promise<ServiceResponseT<ListBrandT>>;
   createBrand(params: CreateBrandParams): Promise<ServiceResponseT<Brand>>;
-  updateBrand(slug: string, params: UpdateBrandParams): Promise<ServiceResponseT<Brand>>;
+  updateBrand(
+    slug: string,
+    params: UpdateBrandParams,
+  ): Promise<ServiceResponseT<Brand>>;
   deleteBrand(slug: string): Promise<ServiceResponseT<null>>;
 }
